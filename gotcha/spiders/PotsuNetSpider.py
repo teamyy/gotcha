70f9348b43847a607b2255ea4f5984c95e5b5784
skyoo2003 <skyoo2003@gmail.com>
2016-05-22 21:21:25 +0900

    팟수넷 수집기 추가 https://github.com/teamyy/gotcha/issues/4

---
+++ gotcha/spiders/PotsuNetSpider.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from gotcha.items import GotchaItem
+
+
+class PotsuNetSpider(CrawlSpider):
+    name = "PotsuNetSpider"
+    allowed_domains = ["potsu.net"]
+    start_urls = [
+        'http://potsu.net/index.php?mid=humor',
+    ]
+
+    rules = (
+        Rule(LinkExtractor(allow=('/index\.php\?.*mid=humor.*&.*document_srl=[0-9]+.*', )), callback='parse_humor'),
+    )
+
+    def parse_humor(self, response):
+        # self.logger.info('Hi, this is an item page! %s', response.url)
+        item = GotchaItem()
+        item['title'] = u"".join(response.xpath('//div[contains(@class, "top_area")]//h1//text()').extract()).strip()
+        item['content'] = u"".join(response.xpath('//div[re:test(@class, "document_.+")]//text()').extract()).strip()
+        item['writer'] = u"".join(response.xpath('//div[contains(@class, "rd_hd")]//a[re:test(@class, "member_.+")]//text()').extract()).strip()
+        item['created_at'] = u"".join(response.xpath('//div[contains(@class, "rd_hd")]//span[contains(@class, "date")]//text()').extract()).strip()
+        item['url'] = response.url
+        item['category'] = 'humor'
+        return item

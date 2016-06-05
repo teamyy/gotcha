e03050d2b45504656196fe5b82c05e94570d6b3d
skyoo2003 <skyoo2003@gmail.com>
2016-06-06 03:43:57 +0900

    debian, centos, redhat 배포판의 경우 필수 시스템 라이브러리 설치 루틴 추가 https://github.com/teamyy/gotcha/issues/29

--- setup.py
+++ setup.py
@@ -2,11 +2,28 @@
 
 import sys
 import os
+import platform
+import subprocess
 
 from setuptools import setup, find_packages
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+system = platform.system()
+if system is not None and not system and system.lower() == 'linux':
+    dist = platform.linux_distribution()[0]
+    if dist is not None and not dist and dist.lower() in ['debian']:
+        install_libs = [
+            'mysql-client', 'mysql-server', 'mysql-utilities', 'libmysqlclient-dev',
+            'python', 'python-dev', 'libxml2-dev', 'libxslt1-dev', 'libffi-dev', 'libssl-dev',
+        ]
+        subprocess.check_call('sudo apt-get install -y %s' % (' '.join(install_libs)))
+    elif dist is not None and not dist and dist.lower() in ['redhat', 'centos']:
+        install_libs = [
+            'mysql', 'mysql-devel', 'python', 'python-devel', 'python-lxml', 'libffi-devel', 'openssl-devel'
+        ]
+        subprocess.check_call('sudo yum install -y %s' % (' '.join(install_libs)))
 
 setup(
     name="gotcha",
@@ -15,14 +32,15 @@ setup(
     description="A crawler to find a lot of humor post",
     license="MIT",
     keywords="gotcha crawler humor article",
-    url="https://github.com/teamyy",
+    url="https://github.com/teamyy/gotcha",
     packages=find_packages(),
     long_description=open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
     install_requires=open(os.path.join(os.path.dirname(__file__), "requirements.txt")).read().splitlines(),
-    classifiers=[  # Ref. https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    license=open(os.path.join(os.path.dirname(__file__), "LICENSE")).read(),
+    classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
         "Topic :: Utilities",
-    ]
+    ]  # Ref. https://pypi.python.org/pypi?%3Aaction=list_classifiers
 )
